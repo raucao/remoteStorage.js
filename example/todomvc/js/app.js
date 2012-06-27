b010e6e0da575df3f10f3142bfbf27b682d42019
Michiel de Jong <michiel@unhosted.org>
2012-06-27 19:12:51 +0300

    todomvc example in developer preview without mvc

--- example/todomvc/js/app.js
+++ example/todomvc/js/app.js
@@ -1,256 +1,233 @@
+var todos,
+  ENTER_KEY = 13;
 
 window.addEventListener( "load", windowLoadHandler, false );
 
 function Todo( title, completed ) {
-    this.id = getUuid();
-    this.title = title;
-    this.completed = completed;
+  this.id = getUuid();
+  this.title = title;
+  this.completed = completed;
 }
 
-var inited=false;
 function windowLoadHandler() {
-    //require(['../../src/remoteStorage'], function(remoteStorage) {
-      //don't know why require executes the callback twice, but it does, so we need this:
-      if(inited) {
-        return;
-      } else {
-        inited = true;
-      }
-      var todos;
-      remoteStorage.displayWidget('remotestorage-connect');
-      remoteStorage.loadModule('tasks', '0.1', 'rw');
-      todos = remoteStorage.tasks.getPrivateList('todos');
-      todos.on('error', function(err) {
-      });
-      todos.on('change', function(id, obj) {
-        refreshData();
-      });
-      refreshData();
-      var ENTER_KEY = 13;
-      document.getElementById( 'new-todo' ).addEventListener( "keypress", newTodoKeyPressHandler, false );
-      document.getElementById( 'toggle-all' ).addEventListener( "change", toggleAllChangeHandler, false );
+  remoteStorage.displayWidget('remotestorage-connect');
+  remoteStorage.loadModule('tasks', '0.1', 'rw');
+  todos = remoteStorage.tasks.getPrivateList('todos');
+  todos.on('error', function(err) {
+  });
+  todos.on('change', function(id, obj) {
+  refreshData();
+  });
+  refreshData();
+  document.getElementById( 'new-todo' ).addEventListener( "keypress", newTodoKeyPressHandler, false );
+  document.getElementById( 'toggle-all' ).addEventListener( "change", toggleAllChangeHandler, false );
+}
+function inputEditTodoKeyPressHandler( event ) {
+  var inputEditTodo,
+  trimmedText,
+  todoId;
 
-      function inputEditTodoKeyPressHandler( event ) {
-          var inputEditTodo,
-              trimmedText,
-              todoId;
+  inputEditTodo = event.target;
+  trimmedText = inputEditTodo.value.trim();
+  todoId = event.target.id.slice( 6 );
 
-          inputEditTodo = event.target;
-          trimmedText = inputEditTodo.value.trim();
-          todoId = event.target.id.slice( 6 );
+  if ( trimmedText ) {
+    if ( event.keyCode === ENTER_KEY ) {
+      todos.set( todoId, trimmedText );
+    }
+  } else {
+    todos.remove( todoId );
+  }
+}
 
-          if ( trimmedText ) {
-              if ( event.keyCode === ENTER_KEY ) {
-                  todos.set( todoId, trimmedText );
-              }
-          } else {
-              todos.remove( todoId );
-          }
-      }
+function inputEditTodoBlurHandler( event ) {
+  var inputEditTodo,
+    todoId;
 
-      function inputEditTodoBlurHandler( event ) {
-          var inputEditTodo,
-              todoId;
+  inputEditTodo = event.target;
+  todoId = event.target.id.slice( 6 );
+  todos.set( todoId, inputEditTodo.value.trim() );
+}
 
-          inputEditTodo = event.target;
-          todoId = event.target.id.slice( 6 );
-          todos.set( todoId, inputEditTodo.value.trim() );
-      }
+function newTodoKeyPressHandler( event ) {
+  if ( event.keyCode === ENTER_KEY ) {
+    var trimmedText = document.getElementById( 'new-todo' ).value;
+    if ( trimmedText ) {
+      todos.add( trimmedText );
+    }
+  }
+}
 
-      function newTodoKeyPressHandler( event ) {
-        if ( event.keyCode === ENTER_KEY ) {
+function toggleAllChangeHandler( event ) {
+  for ( var i in todos.getIds() ) {
+    todos.markAsCompleted( i, event.target.checked);
+  }
+}
 
-          var trimmedText = document.getElementById( 'new-todo' ).value;
-          if ( trimmedText ) {
-            todos.add( trimmedText );
-          }
-        }
-      }
+function spanDeleteClickHandler( event ) {
+  todos.remove( event.target.getAttribute( 'data-todo-id' ) );
+}
 
-      function toggleAllChangeHandler( event ) {
-          for ( var i in todos.getIds() ) {
-              todos.markAsCompleted( i, event.target.checked);
-          }
-      }
+function hrefClearClickHandler() {
+  var ids = todos.getIds();
+  for(var i=0; i<ids.length; i++) {
+    if ( todos.get(ids[i]).completed ) {
+      todos.remove(ids[i]);
+    }
+   }
+}
 
-      function spanDeleteClickHandler( event ) {
-          todos.remove( event.target.getAttribute( 'data-todo-id' ) );
-      }
+function todoContentHandler( event ) {
+  var todoId,
+    div,
+    inputEditTodo;
 
-      function hrefClearClickHandler() {
-          var ids = todos.getIds();
-          for(var i=0; i<ids.length; i++) {
-              if ( todos.get(ids[i]).completed ) {
-                  todos.remove(ids[i]);
-              }
-         }
-      }
+  todoId = event.target.getAttribute( 'data-todo-id' );
+  div = document.getElementById( 'li_'+todoId );
+  div.className = 'editing';
 
-      function todoContentHandler( event ) {
-          var todoId,
-              div,
-              inputEditTodo;
+  inputEditTodo = document.getElementById( 'input_' + todoId );
+  inputEditTodo.focus();
+}
 
-          todoId = event.target.getAttribute( 'data-todo-id' );
-          div = document.getElementById( 'li_'+todoId );
-          div.className = 'editing';
+function checkboxChangeHandler( event ) {
+  var checkbox;
+  checkbox = event.target;
+  todos.markCompleted( checkbox.getAttribute( 'data-todo-id' ), checkbox.checked);
+}
 
-          inputEditTodo = document.getElementById( 'input_' + todoId );
-          inputEditTodo.focus();
-      }
+function refreshData() {
+  var stats = todos.getStats();
+  redrawTodosUI(stats);
+  redrawStatsUI(stats);
+  changeToggleAllCheckboxState(stats);
+}
 
-      function checkboxChangeHandler( event ) {
-          var checkbox;
-          checkbox = event.target;
-          todos.markCompleted( checkbox.getAttribute( 'data-todo-id' ), checkbox.checked);
-      }
+function redrawTodosUI() {
+  var ul,
+    todo,
+    checkbox,
+    label,
+    deleteLink,
+    divDisplay,
+    inputEditTodo,
+    li,
+    i;
 
-      function refreshData() {
-          var stats = todos.getStats();
-          redrawTodosUI(stats);
-          redrawStatsUI(stats);
-          changeToggleAllCheckboxState(stats);
-      }
+  ul = document.getElementById( 'todo-list' );
+  var ids = todos.getIds();
+  document.getElementById( 'main' ).style.display = ids.length ? 'block' : 'none';
 
-      function redrawTodosUI() {
+  ul.innerHTML = "";
+  document.getElementById( 'new-todo' ).value = '';
 
-          var ul,
-              todo,
-              checkbox,
-              label,
-              deleteLink,
-              divDisplay,
-              inputEditTodo,
-              li,
-              i;
+  for ( i= 0; i < ids.length; i++ ) {
+    todo = todos.get(ids[i]);
 
-          ul = document.getElementById( 'todo-list' );
-          var ids = todos.getIds();
-          document.getElementById( 'main' ).style.display = ids.length ? 'block' : 'none';
+    // create checkbox
+    checkbox = document.createElement( 'input' );
+    checkbox.className = 'toggle';
+    checkbox.setAttribute( 'data-todo-id', todo.id );
+    checkbox.type = 'checkbox';
+    checkbox.addEventListener( 'change', checkboxChangeHandler );
 
-          ul.innerHTML = "";
-          document.getElementById( 'new-todo' ).value = '';
+    // create div text
+    label = document.createElement( 'label' );
+    label.setAttribute( 'data-todo-id', todo.id );
+    label.appendChild( document.createTextNode( todo.title ) );
 
-          for ( i= 0; i < ids.length; i++ ) {
-              todo = todos.get(ids[i]);
+    // create delete button
+    deleteLink = document.createElement( 'button' );
+    deleteLink.className = 'destroy';
+    deleteLink.setAttribute( 'data-todo-id', todo.id );
+    deleteLink.addEventListener( 'click', spanDeleteClickHandler );
 
-              // create checkbox
-              checkbox = document.createElement( 'input' );
-              checkbox.className = 'toggle';
-              checkbox.setAttribute( 'data-todo-id', todo.id );
-              checkbox.type = 'checkbox';
-              checkbox.addEventListener( 'change', checkboxChangeHandler );
+    // create divDisplay
+    divDisplay = document.createElement( 'div' );
+    divDisplay.className = 'view';
+    divDisplay.setAttribute( 'data-todo-id', todo.id );
+    divDisplay.appendChild( checkbox );
+    divDisplay.appendChild( label );
+    divDisplay.appendChild( deleteLink );
+    divDisplay.addEventListener( 'dblclick', todoContentHandler );
 
-              // create div text
-              label = document.createElement( 'label' );
-              label.setAttribute( 'data-todo-id', todo.id );
-              label.appendChild( document.createTextNode( todo.title ) );
+    // create todo input
+    inputEditTodo = document.createElement( 'input' );
+    inputEditTodo.id = 'input_' + todo.id;
+    inputEditTodo.className = 'edit';
+    inputEditTodo.value = todo.title;
+    inputEditTodo.addEventListener( 'keypress', inputEditTodoKeyPressHandler );
+    inputEditTodo.addEventListener( 'blur', inputEditTodoBlurHandler );
 
+    // create li
+    li = document.createElement( 'li' );
+    li.id = 'li_' + todo.id;
+    li.appendChild( divDisplay );
+    li.appendChild( inputEditTodo );
 
-              // create delete button
-              deleteLink = document.createElement( 'button' );
-              deleteLink.className = 'destroy';
-              deleteLink.setAttribute( 'data-todo-id', todo.id );
-              deleteLink.addEventListener( 'click', spanDeleteClickHandler );
+    if ( todo.completed )
+    {
+      li.className += 'complete';
+      checkbox.checked = true;
+    }
 
-              // create divDisplay
-              divDisplay = document.createElement( 'div' );
-              divDisplay.className = 'view';
-              divDisplay.setAttribute( 'data-todo-id', todo.id );
-              divDisplay.appendChild( checkbox );
-              divDisplay.appendChild( label );
-              divDisplay.appendChild( deleteLink );
-              divDisplay.addEventListener( 'dblclick', todoContentHandler );
+    ul.appendChild( li );
+  }
+}
 
+function changeToggleAllCheckboxState(stat) {
+  var toggleAll = document.getElementById( 'toggle-all' );
+  if ( stat.todoCompleted === stat.totalTodo ) {
+    toggleAll.checked = true;
+  } else {
+    toggleAll.checked = false;
+  }
+}
 
-              // create todo input
-              inputEditTodo = document.createElement( 'input' );
-              inputEditTodo.id = 'input_' + todo.id;
-              inputEditTodo.className = 'edit';
-              inputEditTodo.value = todo.title;
-              inputEditTodo.addEventListener( 'keypress', inputEditTodoKeyPressHandler );
-              inputEditTodo.addEventListener( 'blur', inputEditTodoBlurHandler );
+function redrawStatsUI(stat) {
+  removeChildren( document.getElementsByTagName( 'footer' )[ 0 ] );
+  document.getElementById( 'footer' ).style.display = stat.totalTodo ? 'block' : 'none';
 
+  if ( stat.todoCompleted > 0 ) {
+    drawTodoClear(stat);
+  }
 
-              // create li
-              li = document.createElement( 'li' );
-              li.id = 'li_' + todo.id;
-              li.appendChild( divDisplay );
-              li.appendChild( inputEditTodo );
+  if ( stat.totalTodo > 0 ) {
+    drawTodoCount(stat);
+  }
+}
 
+function drawTodoCount(stat) {
+  var number,
+    theText,
+    remaining;
+  // create remaining count
+  number = document.createElement( 'strong' );
+  number.innerHTML = stat.todoLeft;
+  theText = ' item';
+  if ( stat.todoLeft !== 1 ) {
+    theText += 's';
+  }
+  theText += ' left';
 
-              if ( todo.completed )
-              {
-                  li.className += 'complete';
-                  checkbox.checked = true;
-              }
+  remaining = document.createElement( 'span' );
+  remaining.id = 'todo-count';
+  remaining.appendChild( number );
+  remaining.appendChild( document.createTextNode( theText ) );
 
-              ul.appendChild( li );
-          }
+  document.getElementsByTagName( 'footer' )[ 0 ].appendChild( remaining );
+}
 
+function drawTodoClear(stat) {
+  var buttonClear = document.createElement( 'button' );
+  buttonClear.id = 'clear-completed';
+  buttonClear.addEventListener( 'click', hrefClearClickHandler );
+  buttonClear.innerHTML = 'Clear completed (' + stat.todoCompleted + ')';
 
-      }
-
-      function changeToggleAllCheckboxState(stat) {
-          var toggleAll = document.getElementById( 'toggle-all' );
-          if ( stat.todoCompleted === stat.totalTodo ) {
-              toggleAll.checked = true;
-          } else {
-              toggleAll.checked = false;
-          }
-      }
-
-      function redrawStatsUI(stat) {
-          removeChildren( document.getElementsByTagName( 'footer' )[ 0 ] );
-          document.getElementById( 'footer' ).style.display = stat.totalTodo ? 'block' : 'none';
-
-          if ( stat.todoCompleted > 0 ) {
-              drawTodoClear(stat);
-          }
-
-          if ( stat.totalTodo > 0 ) {
-              drawTodoCount(stat);
-          }
-      }
-
-      function drawTodoCount(stat) {
-
-          var number,
-              theText,
-              remaining;
-          // create remaining count
-          number = document.createElement( 'strong' );
-          number.innerHTML = stat.todoLeft;
-          theText = ' item';
-          if ( stat.todoLeft !== 1 ) {
-              theText += 's';
-          }
-          theText += ' left';
-
-          remaining = document.createElement( 'span' );
-          remaining.id = 'todo-count';
-          remaining.appendChild( number );
-          remaining.appendChild( document.createTextNode( theText ) );
-
-          document.getElementsByTagName( 'footer' )[ 0 ].appendChild( remaining );
-      }
-
-      function drawTodoClear(stat) {
-
-          var buttonClear = document.createElement( 'button' );
-          buttonClear.id = 'clear-completed';
-          buttonClear.addEventListener( 'click', hrefClearClickHandler );
-          buttonClear.innerHTML = 'Clear completed (' + stat.todoCompleted + ')';
+  document.getElementsByTagName( 'footer' )[ 0 ].appendChild( buttonClear );
+}
 
 
-          document.getElementsByTagName( 'footer' )[ 0 ].appendChild( buttonClear );
-      }
-
-
-      function removeChildren( node ) {
-          node.innerHTML = '';
-      }
-
-
-    //});
+function removeChildren( node ) {
+  node.innerHTML = '';
 }
