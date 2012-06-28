eb43e0501603add6f0134b09ce2b34eca196b4ad
Michiel de Jong <michiel@unhosted.org>
2012-06-28 23:05:28 +0300

    first draft of sync nodes tree

--- src/lib/sync.js
+++ src/lib/sync.js
@@ -1,23 +1,99 @@
 define(['./wireClient', './session', './store'], function(wireClient, session, store) {
-  var prefix = '_remoteStorage_';
-  function addToList(list, path, value) {
-    var list, listStr = localStorage.getItem(prefix+list);
+  var prefix = '_remoteStorage_', busy=false;
+   
+  function addToList(listName, path, value) {
+    var list = getList(listName);
+    if(list[path] != value) {
+      list[path] = value;
+      localStorage.setItem(prefix+listName, JSON.stringify(list));
+    }
+  }
+  function getList(listName) {
+    var list, listStr = localStorage.getItem(prefix+listName);
     if(listStr) {
       try {
-        list = JSON.parse(listStr);
+        return JSON.parse(listStr);
       } catch(e) {
       }
     }
-    if(!list) {
-      list = {};
-    }
-    if(list[path] != value) {
-      list[path] = value;
-      localStorage.setItem(prefix+list, JSON.stringify(list));
-    }
+    return {};
   }
   function getState(path) {
-    return 'busy';
+    if(session.getState() == 'connected') {
+      if(busy) {
+        return 'busy';
+      } else {
+        return 'connected';
+      }
+    } else {
+      return 'anonymous';
+    }
+  }
+  //as you pull in the new timestamps for a directory, you can always update the local copy, because the values in there are always from the last pull.
+  //additionally, the ones that didn't change, you will not have to check. the ones that are newer than what we have should be added to the 'news' list.
+  //we should start syncing with the least common prefix of all pull paths.
+  //start: synced: '', pulling: '/', dirty: '/a/b', '/a/v/d', '/a/v/e'
+  //pull '/', compare with current. if 'a/' gets updated, we pull /a/:
+  //synced: '/', pulling: 'a/', dirty: '/a/b', '/a/v/d', '/a/v/e'
+  //if 'a/' gets updated,
+  //synced: '/a/', pulling: '/a/b/', dirty: '/a/b', '/a/v/d', '/a/v/e'
+  //if 'b' gets updated,
+  //synced: '/a/b', pulling: '/a/v/', dirty: '/a/v/d', '/a/v/e'
+  //if 'd' gets updated but 'e' doesn't,
+  //synced: ['/a/b', '/a/v/e'], pulling: '/a/v/d', dirty: '/a/v/d'
+  //
+  //we have a list of paths that should be kept in sync, mapped to the last time they were synced.
+  //- take all containing dirs of paths in that list, and make sure they are there too.
+  function containingDir(path) {
+    var pathParts = path.split('/');
+    if(path.substr(-1)=='/') {
+      pathParts.pop();
+    }
+    if(pathParts.length) {
+      pathParts.pop();
+      return pathParts.join('/');
+    }
+  }
+  function buildTree(listName) {
+    var list = getList(listName);
+    var changed = false;
+    do {
+      for(var i in nodes) {
+        var containingDir = getContainingDir(i);
+        if(containingDir && !list[containingDir]) {
+          if(!list[containingDir]) {
+            list[containingDir] = {
+              path: containingDir,
+              chidren: []
+            };
+          }
+          list[containingDir].children.push(node);
+        }
+      }
+    } while(changed);
+    var nodes = [];
+    for(var i in list) {
+      nodes.push(i);
+    }
+    return nodes.sort(function(a, b) {
+      return (a.length - b.length);
+    });
+  }
+  function pull() {
+    var pullTree = buildTree('pull');//list of node objects. each node has a .path and a .children.
+    for(var i=0; i<pullTree.length; i++) {
+      pullNode(list[i]);
+    }
+  }
+  function pullNode(node) {
+    wireClient.get(node.path, function(err, data) {
+      var changedElts = handleIncoming(node.path, data);
+      for(var i in node.children) {//this will launch concurrent requests
+        if(changeElts.indexOf(i)!=-1) {
+          pullNode(children[i]);
+        }
+      }
+    });
   }
   function getUserAddress() {
     return null;
