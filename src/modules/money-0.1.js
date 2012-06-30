f50fb961f39999faf417a34ec16a10108f39cf22
Michiel de Jong <michiel@unhosted.org>
2012-06-30 11:06:03 +0300

    adding expenses example

---
+++ src/modules/money-0.1.js
@@ -0,0 +1,44 @@
+remoteStorage.defineModule('money', '0.1', function(myBaseClient) {
+  var balances = {
+    'unhosted-project':0,
+    'unhosted-reserved':0,
+    'michiel':0,
+    'javier':0,
+    'jan':0,
+    'hugo':0
+  };
+  function roundOff(amount) {
+    return Math.floor(100*amount+0.5)/100;
+  }
+  function itoa(i) {
+    var wholes = Math.floor(i);
+    var cents = (i - Math.floor(i))*100;
+    var dimes = Math.floor(cents/10);
+    cents = Math.floor(cents - 10*dimes+0.5);
+    //console.log(i+' -> '+wholes+'.'+dimes+cents);
+    return ''+wholes+'.'+dimes+cents;
+  }
+  function display(name, items, owee, ower) {
+    var sum=0;
+    for(var thing in items) {
+      console.log(' >>> '+thing+': '+itoa(items[thing])+' EUR');
+      sum += items[thing];
+    }
+    sum = roundOff(sum);
+    console.log(name+': '+itoa(sum)+' EUR');
+    if(owee && ower) {
+      balances[owee]+=sum;
+      balances[ower]-=sum;
+    }
+    return sum;
+  }
+  function displayBalances() {
+    for(var i in balances) {
+      console.log(i+': '+itoa(roundOff(balances[i]))+' EUR');
+    }
+  }
+  return {
+    display: display,
+    displayBalances: displayBalances
+  };
+});
